94a6058c2fd5a0e162d3130edbc3855c397c3a67
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-05-09 10:40:42 -0500

    Update rocket launch icon to secondary color and add loading spinner to update button

--- src/pages/updateApp.vue
+++ src/pages/updateApp.vue
@@ -2,7 +2,7 @@
   <div class="tw-flex tw-justify-center tw-items-center tw-bg-gray-100 tw-h-screen">
     <div class="tw-w-fit tw-bg-white tw-p-6 tw-mx-5 tw-rounded-xl tw-shadow-lg">
       <div class="text-center">
-        <q-icon name="fa-regular fa-rocket-launch" size="xl" color="green" />
+        <q-icon name="fa-regular fa-rocket-launch" size="xl" color="secondary" />
         <div class="tw-text-2xl tw-font-bold tw-my-5 text-blue">
           {{ i18n.tr('isite.cms.messages.updateAvailable') }}
         </div>
@@ -12,11 +12,15 @@
       </p>
       <div class="tw-flex tw-justify-center tw-w-full">
         <q-btn unelevated @click="update" color="primary"
+            :loading="isLoading" :disable="isLoading"
                class="tw-mt-5 tw-m-auto tw-rounded-lg">
           <span>
             {{ i18n.tr('isite.cms.messages.updateNow') }}
             <span v-if="countdown > 0"> ({{ countdown }})</span>
           </span>
+          <template v-slot:loading>
+            <i class="fa-solid fa-spinner-third fa-spin"></i>
+          </template>
         </q-btn>
       </div>
     </div>
@@ -30,6 +34,7 @@ import { i18n, store, router, cache } from 'src/plugins/utils';
 
 const route = useRoute();
 const countdown = ref(15);
+const isLoading = ref(false);
 let interval;
 
 onBeforeMount(async () =>
@@ -53,10 +58,13 @@ const update = async () =>
 {
   clearInterval(interval);
   router.push({ query: { ...route.query, updated: '1' } });
-  await store.dispatch('qsiteApp/REFRESH_PAGE');
-  await store.dispatch('qsiteApp/CLEAR_CACHE_STORAGE');
-  await store.dispatch('qsiteApp/DELETE_SW');
-  //Let this after success refresh
+  isLoading.value = true
+  await Promise.allSettled([
+    await store.dispatch('qsiteApp/REFRESH_PAGE'),
+    await store.dispatch('qsiteApp/CLEAR_CACHE_STORAGE'),
+    await store.dispatch('qsiteApp/DELETE_SW'),
+  ])
+  isLoading.value = false
   if (route.query.version) await cache.set('api.version', route.query.version);
   window.location.reload();
 };
