6ead8b7ba0f2ae15a02663551b3f38d8bd61cc59
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-06-12 16:21:14 -0500

    Refactor cacheOffline and paginateCacheOffline. Refactor cacheOffline class to improve data retrieval and manipulation.

--- src/plugins/cacheOffline.js
+++ src/plugins/cacheOffline.js
@@ -2,73 +2,104 @@ import { cache } from 'src/plugins/utils';
 
 class cacheOffline {
 
+  async getDataIntoObject(apiRoute = null) {
+    if (!apiRoute) return [];
+
+    const TAG = '::offline';
+    let route = [ apiRoute ];
+    
+    if (!apiRoute.includes(TAG) && !apiRoute.includes('::requestParams')) {
+      const keys = await cache.keys()
+      route = keys.filter(key => key.includes(apiRoute));
+      const matchingSpecialKey = route.filter(key => key.includes(TAG));
+      if (matchingSpecialKey.length > 0) route = matchingSpecialKey;
+    }
+
+    return await cache.get.items(route) || { data: [] }
+  }
+
+  async getAllList(apiRoute = null) {
+    const data = []
+    const cacheResponse = await this.getDataIntoObject(apiRoute);
+    
+    Object.keys(cacheResponse).map(key => { 
+      if (Array.isArray(cacheResponse[key].data)) data.push(...cacheResponse[key].data) 
+    })
+
+    return { data };
+  }
+
   async addNewRecord(apiRoute = null, data = {}) {
-    if (!apiRoute) return [];
-    const route = `${apiRoute}::offline`;
-    const cacheResponse = await cache.get.item(route) || { data: [] };
-    await cache.remove(apiRoute);
-    await cacheResponse.data.unshift({ ...data });
-    await cache.set(route, cacheResponse);
-    return true;
-  }
-
-  async updateRecord(apiRoute = null, data = {}, id) {
-    if (!apiRoute) return [];
-    const route = `${apiRoute}::offline`;
-    const cacheResponse = await cache.get.item(route) || { data: [] };
-
+    const route = `${apiRoute}::offline`
+    const cacheResponse = await this.getAllList(route);
     const dataInCache = structuredClone(cacheResponse);
-    const records = await dataInCache.data.map(item =>
-      String(item.id) === String(id) ? { ...item, ...data } : item
-    );
-    const newData = {
-      ...dataInCache,
-      data: records
-    };
-    await cache.set(route, { ...newData });
-    return true;
-  }
-
-  async updateList(apiRoute = null, data) {
-    if (!apiRoute) return [];
-    const route = `${apiRoute}::offline`;
-    const cacheResponse = await cache.get.item(route) || { data: [] };
-
-    const dataInCache = structuredClone(cacheResponse);
-    dataInCache.data.push({ ...data });
+    
+    await cache.remove(route);
+    dataInCache.data.unshift({ ...data });
     await cache.set(route, dataInCache);
     return true;
   }
 
-  async deleteItem(itemId, apiRoute = null) {
-    if (!apiRoute) return [];
-    const route = `${apiRoute}::offline`;
-    const cacheResponse = await cache.get.item(route) || { data: [] };
-
+  async updateRecord(apiRoute = null, data = {}, id) {
+    const cacheResponse = await this.getDataIntoObject(apiRoute);
     const dataInCache = structuredClone(cacheResponse);
-    const index = dataInCache.data.findIndex(
-      item => String(item.id) === String(itemId)
-    );
-    if (index < 0) return null;
-    dataInCache.data.splice(index, 1);
-    await cache.set(route, { ...dataInCache });
+    let recordUpdate = {}
+
+    Object.keys(cacheResponse).map(async key => {
+      if (!Array.isArray(dataInCache[key].data)) return;
+      const records = await dataInCache[key].data.map(item => {
+        if (String(item.id) === String(id)) {
+          recordUpdate = { ...item, ...data }
+          return recordUpdate
+        } else return item
+      });
+      const newData = {
+        ...dataInCache[key],
+        data: records
+      };
+      await cache.set(key, { ...newData });
+    });
+
+    return recordUpdate;
+  }
+
+  async updateList(apiRoute = null, data) {
+    const cacheResponse = await this.getDataIntoObject(apiRoute);
+    const dataInCache = structuredClone(cacheResponse);
+
+    Object.keys(dataInCache).map(async key => {
+      if (!Array.isArray(dataInCache[key].data)) return;
+      dataInCache[key].data.push({ ...data });
+      await cache.set(key, { ...dataInCache[key] });
+    });
+
     return true;
   }
 
-  async getAllList(apiRoute = null) {
-    if (!apiRoute) return [];
-    const route = `${apiRoute}::offline`;
-    return await cache.get.item(route) || { data: [] };
+  async deleteItem(itemId, apiRoute = null) {
+    const cacheResponse = await this.getDataIntoObject(apiRoute);
+    const dataInCache = structuredClone(cacheResponse);
+
+    Object.keys(dataInCache).map(async key => {
+      if (!Array.isArray(dataInCache[key].data)) return;
+      const index = dataInCache[key].data.findIndex(
+        item => String(item.id) === String(itemId)
+      );
+      if (index < 0) return null;
+      dataInCache[key].data.splice(index, 1);
+      await cache.set(key, { ...dataInCache[key] });
+    });
+
+    return true;
   }
 
   async getItemById(itemId, apiRoute = null) {
-    if (!apiRoute) return [];
-    const route = `${apiRoute}::offline`;
-    const cacheResponse = await cache.get.item(route) || { data: [] };
+    const cacheResponse = await this.getAllList(apiRoute);
+    
     const record = await cacheResponse.data.find(
       item => item.id === itemId
     );
-    return record;
+    return record || { data: [] }
   }
 }
 
