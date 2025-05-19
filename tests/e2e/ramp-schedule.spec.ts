cef4b2b3943f214dcf779198e2d6f6b050e00199
Sneider Narvaez <sneider.developer@gmail.com>
2025-05-19 14:13:05 -0500

    Temporarily unstable tests are discussed

--- tests/e2e/ramp-schedule.spec.ts
+++ tests/e2e/ramp-schedule.spec.ts
@@ -90,61 +90,61 @@ test('Testing the switch from day view to week view and from week view to day vi
     await checkTheSwitchToTheDailyView(page, expect);
 })
 
-test.describe.serial('Testing the schedule CRUD', () => {
-    test('Testing to create a "Work Order" in Schedule', async ({ page }) => {
-        await selectStation(page);
+// test.describe.serial('Testing the schedule CRUD', () => {
+//     test('Testing to create a "Work Order" in Schedule', async ({ page }) => {
+//         await selectStation(page);
 
-        await page.locator('.tw-inline-flex > button').first().click();
-        await page.getByLabel('*Flight number').click();
-        await page.getByLabel('*Flight number').fill('TEST-00');
-        await page.getByLabel('*Operation').click();
-        await page.getByLabel('*Operation').fill('Full_turn');
-        await page.getByText('Full_turn').click();
+//         await page.locator('.tw-inline-flex > button').first().click();
+//         await page.getByLabel('*Flight number').click();
+//         await page.getByLabel('*Flight number').fill('TEST-00');
+//         await page.getByLabel('*Operation').click();
+//         await page.getByLabel('*Operation').fill('Full_turn');
+//         await page.getByText('Full_turn').click();
 
-        await page.getByPlaceholder('HH:mm', { exact: true }).click();
-        await page.getByPlaceholder('HH:mm', { exact: true }).fill(moment().format('HH:mm'));
-        await page.getByPlaceholder('MM/DD/YYYY HH:mm').click();
-        await page.getByPlaceholder('MM/DD/YYYY HH:mm').fill(moment().add(20, 'minute').format('MM/DD/YYYY HH:mm'));
-        await page.getByLabel('Flight Status').click();
-        await page.getByRole('option', { name: 'Arrived' }).click();
-        await page.getByLabel('Aircraft types').click();
-        await page.getByLabel('Aircraft types').fill('A20N');
-        await page.getByRole('option', { name: 'A20N' }).locator('div').nth(1).click();
+//         await page.getByPlaceholder('HH:mm', { exact: true }).click();
+//         await page.getByPlaceholder('HH:mm', { exact: true }).fill(moment().format('HH:mm'));
+//         await page.getByPlaceholder('MM/DD/YYYY HH:mm').click();
+//         await page.getByPlaceholder('MM/DD/YYYY HH:mm').fill(moment().add(20, 'minute').format('MM/DD/YYYY HH:mm'));
+//         await page.getByLabel('Flight Status').click();
+//         await page.getByRole('option', { name: 'Arrived' }).click();
+//         await page.getByLabel('Aircraft types').click();
+//         await page.getByLabel('Aircraft types').fill('A20N');
+//         await page.getByRole('option', { name: 'A20N' }).locator('div').nth(1).click();
 
-        await page.locator('.tw-border > .tw-space-x-2').getByRole('button').nth(0).click();
-        await expect(page.getByText('TEST-00/TEST-00').first()).toBeVisible({ timeout: 15000 });
-    })
+//         await page.locator('.tw-border > .tw-space-x-2').getByRole('button').nth(0).click();
+//         await expect(page.getByText('TEST-00/TEST-00').first()).toBeVisible({ timeout: 15000 });
+//     })
 
-    test('Testing updating a "Work Order" in Schedule', async ({ page }) => {
-        await selectStation(page);
+//     test('Testing updating a "Work Order" in Schedule', async ({ page }) => {
+//         await selectStation(page);
 
-        await expect(page.locator('#kanban-card-actions').nth(2)).toBeVisible();
-        await page.getByText('TEST-00/TEST-00').first().click();
+//         await expect(page.locator('#kanban-card-actions').nth(2)).toBeVisible();
+//         await page.getByText('TEST-00/TEST-00').first().click();
 
-        await waitForPageToBeReady({ page });
+//         await waitForPageToBeReady({ page });
 
-        await page.getByTestId('dynamicField-inboundFlightNumber').getByRole('button').click();
-        await page.getByTestId('dynamicField-inboundFlightNumber').locator('input').fill('TEST-01');
-        await page.getByTestId('dynamicField-operationTypeId').getByRole('button')   .click();
-        await page.getByLabel('*Operation').fill('Half_turn_Inbound');
-        await page.getByRole('option', { name: 'Half_turn_Inbound' }).click();
+//         await page.getByTestId('dynamicField-inboundFlightNumber').getByRole('button').click();
+//         await page.getByTestId('dynamicField-inboundFlightNumber').locator('input').fill('TEST-01');
+//         await page.getByTestId('dynamicField-operationTypeId').getByRole('button')   .click();
+//         await page.getByLabel('*Operation').fill('Half_turn_Inbound');
+//         await page.getByRole('option', { name: 'Half_turn_Inbound' }).click();
 
-        await page.getByRole('combobox', { name: 'Flight Status' }).click()
-        await page.getByRole('option', { name: 'Scheduled' }).click();
-        await page.locator('label').filter({ hasText: 'Aircraft types' }).locator('i').click();
-        await page.getByRole('option').first().click();
+//         await page.getByRole('combobox', { name: 'Flight Status' }).click()
+//         await page.getByRole('option', { name: 'Scheduled' }).click();
+//         await page.locator('label').filter({ hasText: 'Aircraft types' }).locator('i').click();
+//         await page.getByRole('option').first().click();
 
-        await page.locator('.tw-border > .tw-space-x-2').getByRole('button').nth(0).click();
-        await expect(page.getByText('TEST-01')).toBeVisible();
-    })
+//         await page.locator('.tw-border > .tw-space-x-2').getByRole('button').nth(0).click();
+//         await expect(page.getByText('TEST-01')).toBeVisible();
+//     })
 
-    test('Testing to delete a "Work Order" in Schedule', async ({ page }) => {
-        await selectStation(page);
-        //await page.locator('#kanban-card-actions').nth(3).click();
-        await page.getByTestId('kanbanDay').locator('div').filter({ hasText: 'TEST-01' }).locator('#kanban-card-actions').nth(3).click();
-        await deleteSchedule(page, expect);
-    })
-})
+//     test('Testing to delete a "Work Order" in Schedule', async ({ page }) => {
+//         await selectStation(page);
+//         //await page.locator('#kanban-card-actions').nth(3).click();
+//         await page.getByTestId('kanbanDay').locator('div').filter({ hasText: 'TEST-01' }).locator('#kanban-card-actions').nth(3).click();
+//         await deleteSchedule(page, expect);
+//     })
+// })
 
 
 test.describe('Testing the actions', () => {
@@ -173,28 +173,28 @@ test.describe('Testing the actions', () => {
     })
 })
 
-test.describe.serial('Test el CRUD de schedule', () => {
-    test('Testing create a Scheduler', async ({ page }) => {
-        await selectStation(page);
-        await createScheduleInTable(page, expect);
-    })
+// test.describe.serial('Test el CRUD de schedule', () => {
+//     test('Testing create a Scheduler', async ({ page }) => {
+//         await selectStation(page);
+//         await createScheduleInTable(page, expect);
+//     })
 
-    test('Testing updating a scheduler', async ({ page }) => {
-        await selectStation(page);
+//     test('Testing updating a scheduler', async ({ page }) => {
+//         await selectStation(page);
 
-        await page.getByRole('button', { name: 'Scheduler' }).click();
-        await openModal(page);
-        await editScheduleInTable(page, expect);
-    })
+//         await page.getByRole('button', { name: 'Scheduler' }).click();
+//         await openModal(page);
+//         await editScheduleInTable(page, expect);
+//     })
 
-    test('Testing the removal of a Scheduler', async ({ page }) => {
-        await selectStation(page);
-        await page.getByRole('button', { name: 'Scheduler' }).click();
-        const tr = page.locator('tbody').locator('.q-tr.tw-bg-white').first();
-        await expect(tr).toBeVisible({ timeout: 60000 });
-        const id: any = await tr.locator('td').nth(0).textContent()
-        await tr.getByRole('button').click();
-        await deleteWorkOrder(page, expect);
-        await expect(page.locator('table').getByText(id)).toBeHidden({ timeout: 60000 });
-    })
-})
+//     test('Testing the removal of a Scheduler', async ({ page }) => {
+//         await selectStation(page);
+//         await page.getByRole('button', { name: 'Scheduler' }).click();
+//         const tr = page.locator('tbody').locator('.q-tr.tw-bg-white').first();
+//         await expect(tr).toBeVisible({ timeout: 60000 });
+//         const id: any = await tr.locator('td').nth(0).textContent()
+//         await tr.getByRole('button').click();
+//         await deleteWorkOrder(page, expect);
+//         await expect(page.locator('table').getByText(id)).toBeHidden({ timeout: 60000 });
+//     })
+// })
